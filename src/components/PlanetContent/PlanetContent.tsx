3178cea645ac728b7f5532f7e3193d027aee7f2e
Oleksandr-Berd <alex.berd86@gmail.com>
2023-06-23 19:46:58 +0300

    made markup for mobile overview planets

--- src/components/PlanetContent/PlanetContent.tsx
+++ src/components/PlanetContent/PlanetContent.tsx
@@ -1,26 +1,57 @@
+import { NavLink } from "react-router-dom";
 import { planetsSvg, planetsSvgMobile } from "utils/processedImages";
+import externalPath from "assets/images/external-link-square-with-an-arrow-in-right-diagonal.png"
 
 
 interface IProps {
     topic: string,
-    content: { [key: string]: any; } | undefined,
+    content: { [key: string]:any } | undefined,
 }
 
 const PlanetContent = ({ topic, content }: IProps) => {
 
     
 
-    const ImageComponentMobile: any = planetsSvgMobile[content!.images.planet]
+    const ImageComponentMobile = planetsSvgMobile[content!.images.planet]
     
+    const ImageComponentDesktop = planetsSvg[content!.images.planet]
+
     
 
     return (<div>
-        {topic === "overview" ?
+        {topic === "overview" ?<>
             <ImageComponentMobile />
+            <h2>{content!.name}</h2>
+            <p>{content!.overview.content}</p>
+            <div>
+                <p>Source:</p>
+                <NavLink to={content!.overview.source}><span>Wikipedia</span><img src={externalPath} alt="externalPath" /></NavLink>
+                
+            </div>
+           
+        </>
 : topic === "structure" ?
                 <div style={{ color: "red" }}>{topic}</div>
                 : 
                 <div style={{ color: "red" }}>{topic}</div>}
+        <ul>
+            <li>
+                <h4>rotation time</h4>
+                <p>{content!.rotation}</p>
+            </li>
+            <li>
+                <h4>revolution time</h4>
+                <p>{content!.revolution}</p>
+            </li>
+            <li>
+                <h4>radius</h4>
+                <p>{content!.radius}</p>
+            </li>
+            <li>
+                <h4>average temp.</h4>
+                <p>{content!.temperature}</p>
+            </li>
+        </ul>
     </div> );
 }
  
